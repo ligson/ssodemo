77adfc25216bf0e0bace771b9796a4c30c5f20d2
ligson <593949938@qq.com>
2015-11-13 14:01:51 +0800

    公钥私钥生成，还原工具

--- auth/src/org/ligson/ssodemo/auth/KeyUtils.java
+++ auth/src/org/ligson/ssodemo/auth/KeyUtils.java
@@ -2,22 +2,28 @@ package org.ligson.ssodemo.auth;
 
 import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * Created by ligson on 2015/11/13.
+ * RSA软加密key生成还原工具,硬key不可以使用
  */
 public class KeyUtils {
+    private static final String ALG_NAME = "RSA";
+
+    /***
+     * 生成RSA密钥对
+     *
+     * @return [pubKey:公钥base64,priKey:私钥base64]或者null
+     */
     public static Map<String, String> generateKey() {
         Map<String, String> map = new HashMap<String, String>();
-        KeyPairGenerator generator = null;
+        KeyPairGenerator generator;
         try {
-            generator = KeyPairGenerator.getInstance("RSA");
+            generator = KeyPairGenerator.getInstance(ALG_NAME);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -34,26 +40,36 @@ public class KeyUtils {
         return map;
     }
 
+    /***
+     * pkcs8形式的私钥还原
+     *
+     * @param priKey 私钥的base64字符串
+     * @return 私钥对象或者null
+     */
     public static PrivateKey decodePrivateKey(String priKey) {
         byte[] buf = Base64.getDecoder().decode(priKey);
         try {
             PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buf);
-            KeyFactory factory = KeyFactory.getInstance("RSA");
-            PrivateKey privateKey = factory.generatePrivate(keySpec);
-            return privateKey;
+            KeyFactory factory = KeyFactory.getInstance(ALG_NAME);
+            return factory.generatePrivate(keySpec);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static PublicKey decodePublicKey(String pubKey){
+    /***
+     * x509证书公钥还原
+     *
+     * @param pubKey 公钥的base64字符串
+     * @return 公钥对象或者null
+     */
+    public static PublicKey decodePublicKey(String pubKey) {
         byte[] buf = Base64.getDecoder().decode(pubKey);
         X509EncodedKeySpec encodedKeySpec = new X509EncodedKeySpec(buf);
         try {
-            KeyFactory factory = KeyFactory.getInstance("RSA");
-            PublicKey publicKey = factory.generatePublic(encodedKeySpec);
-            return publicKey;
+            KeyFactory factory = KeyFactory.getInstance(ALG_NAME);
+            return factory.generatePublic(encodedKeySpec);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +78,9 @@ public class KeyUtils {
 
 
     public static void main(String[] args) throws Exception {
-        Map<String,String> map = generateKey();
+        Map<String, String> map = generateKey();
+
+        assert map != null;
         String pubKey = map.get("pubKey");
         String priKey = map.get("priKey");
         System.out.println(pubKey);
@@ -71,6 +89,8 @@ public class KeyUtils {
         PublicKey publicKey = decodePublicKey(pubKey);
         PrivateKey privateKey = decodePrivateKey(priKey);
 
+        assert publicKey != null;
+        assert privateKey != null;
         String pubKey2 = Base64.getEncoder().encodeToString(publicKey.getEncoded());
         String priKey2 = Base64.getEncoder().encodeToString(privateKey.getEncoded());
 
